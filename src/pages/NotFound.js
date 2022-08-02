e3d55aa164f0c0b474de94726d5dec25cb04d4b5
pkamano <p.amano1225@protonmail.com>
2022-08-02 13:34:07 -0700

    Header Footer 404 Styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,27 @@
 import React, { Component} from 'react'
 class NotFound extends Component {
     render() {
+        const bckgrdmystyle = {
+            backgroundImage: "url(/wyc.jpeg)",
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "center",
+            height: "100vh",
+        };
+        const fontStyle = {
+            textColor: "white",
+            padding: "10px",
+            fontFamily: "Arial",
+            fontSize: "46pt",
+            textAlign: "center",
+            backgroundColor: "pink",
+          };
         return (
 
             <div class="notfound">
             <div class="notfound-404">
-            <h1>404</h1>
-            <h2>Page not found</h2>
+            <h1 style={fontStyle}>404</h1>
+            <h2 style={bckgrdmystyle}>Page not found</h2>
             </div>
             <a href="#" class="btn btn-primary">Go To Homepage</a>
             </div>
