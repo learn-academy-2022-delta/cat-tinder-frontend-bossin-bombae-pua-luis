d67beade049fa2d2f890823099e75370258ebddf
ninerboy81 <luis.ramos07@yahoo.com>
2022-08-02 08:49:18 -0700

    fixed indentation on header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React, { Component} from 'react'
 class Header extends Component {
     render() {
         return (
-            <div class="header">
-            <a href="#default" class="logo">CompanyLogo</a>
+    
+        <div class="header">
+                <a href="#default" class="logo">CompanyLogo</a>
             <div class="header-right">
-            <a class="active" href="#home">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
+                <a class="active" href="#home">Home</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
         </div>
-</div>
         )
     }
 }
